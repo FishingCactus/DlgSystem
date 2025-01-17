c9910d8e9fbfdcb031331081ce8294480da7b370
michael.delva <michael.delva@fishingcactus.com>
2025-01-17 09:48:04 +0100

    Fixed compilation for UE 5.5

--- Source/DlgSystem/IO/DlgJsonParser.cpp
+++ Source/DlgSystem/IO/DlgJsonParser.cpp
@@ -740,7 +740,7 @@ bool FDlgJsonParser::JsonValueToProperty(const TSharedPtr<FJsonValue>& JsonValue
 	for (int32 Index = 0; Index < ItemsToRead; ++Index)
 	{
 		// ValuePtr + Index * Property->ElementSize is literally FScriptArrayHelper::GetRawPtr
-		bReturnStatus &= ConvertScalarJsonValueToProperty(ArrayValue[Index], Property, ContainerPtr, ValueIntPtr + Index * Property->ElementSize);
+		bReturnStatus &= ConvertScalarJsonValueToProperty(ArrayValue[Index], Property, ContainerPtr, ValueIntPtr + Index * Property->GetElementSize());
 	}
 	return bReturnStatus;
 }
